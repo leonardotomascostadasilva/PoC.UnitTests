29af132a8cedae919dff2e364b44404f0bf67e73
Leonardo <Leonardo@DESKTOP-AAJA3AB>
2025-07-15 02:26:55 -0300

    automatic commit

--- UnitTests/Respositories/Test2.cs
+++ UnitTests/Respositories/Test2.cs
@@ -1,15 +1,45 @@
 ﻿//[assembly: CollectionBehavior(Parallelization = true)]
+using Moq;
+using PoC.UnitTests.Wrapper;
+using System.Runtime.CompilerServices;
+
 namespace UnitTests.Respositories
 {
    // [Collection("MySerialTests")]
     public class MyTests1
     {
+        private readonly Mock<IDapperWrapper> dapperWrapper;
+
+        public MyTests1()
+        {
+            this.dapperWrapper = new Mock<IDapperWrapper>();
+        }
+
         [Fact]
         public void Test1()
         {
             Console.WriteLine("Test1 START");
             Thread.Sleep(3000);
             Console.WriteLine("Test1 END");
+        }
+
+
+        [Fact]
+        public void T1()
+        {
+            throw new NotImplementedException($"{RuntimeHelpers.GetHashCode(dapperWrapper.Object)}");
+        }
+
+        [Fact]
+        public void T2()
+        {
+            throw new NotImplementedException($"{RuntimeHelpers.GetHashCode(dapperWrapper.Object)}");
+        }
+
+        [Fact]
+        public void T3()
+        {
+            throw new NotImplementedException($"{RuntimeHelpers.GetHashCode(dapperWrapper.Object)}");
         }
     }
 
